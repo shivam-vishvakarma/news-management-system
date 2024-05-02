5e13d2da14377cd7f63547eea115eaef6bd818bb
ravishankarsharma123 <ravishankarsharma1006@gmail.com>
2024-05-02 11:25:12 +0530

    last commit

--- Django_server/server/main/views.py
+++ Django_server/server/main/views.py
@@ -78,7 +78,7 @@ class ArticleViewSet(ModelViewSet):
         else:
             request.data['articalId'] = pk
             request.data['userId'] = request.user.id
-            serializer = CommentSerializer(data=request.data)
+            serializer = CommentSerializer(data=request.data)n  
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=201)
